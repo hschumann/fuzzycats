04501e0f7b55c553e56d0aee4809534ad09b8318
hschumann <hschuman@calpoly.edu>
2017-11-28 16:11:50 -0800

    Add files via upload

--- fuzzyCats.py
+++ fuzzyCats.py
@@ -9,21 +9,49 @@ crime = pd.read_csv('C:/Users/Owner/Downloads/Crime_Data_Chicago.csv',low_memory
 crime['latitude'] = np.array([float(s.split(',')[0][1:]) for s in crime['Location ']])
 crime['longitude'] = np.array([float(s.split(',')[1][:-1]) for s in crime['Location ']])
 
+## rescale time variable
+def newTimes(df):
+	df['Time Occurred'][np.where(df['Time Occurred'] < 800)] = 2400 - (800 - df['Time Occurred'])
+	df['Time Occurred'][np.where(df['Time Occurred'] >= 800)] = df['Time Occurred'] - 800
+
+## make sex binary!
+def newSex(df):
+	if df['Victim Sex'] == 'M':
+		df['Sex'] = 1
+	else:
+		df['Sex'] = 0
+
+## clean age (remove missing values)
+def cleanAge(df):
+	if df['Victim Age'] == '':
+		df['Age'] = np.mean(df['Victim Age'])
+	
+## normalize all the variables
+def normalizeDF(df,columns):
+	for col in columns:
+		df[col] = df[col] / np.max(df[col])
+
+## distance formula
 def L2Norm(pt1,pt2):
 	distance = 0.0
 	for var in pt1.columns:
-		try:
-			distance += (pt1[var] - pt2[var]) ** 2
-		except:
-			if pt1[var] != pt2[var]:
-				distance += 1
+		distance += (pt1[var] - pt2[var]) ** 2
 	return distance ** 0.5
 
-def getCentroids(df):
-	for col in df.columns:
-		np.sum(df[groupNum == ][col])
+def L1Norm(pt1,pt2):
+	distance = 0.0
+	for var in pt1.columns:
+		distance += abs(pt1[var] - pt2[var])
+	return distance
+
+## getting the new centroids
+def getCentroids(df,k):
+	for i in range(k):
+		for col in df.columns:
+			cent[i][col] = np.sum(df[col]) / len(df[col])
 	return cent
 
+## classifies points
 def classifyPoint(centroids,point):
 	smallestDist = np.inf
 	closest = 0
@@ -35,6 +63,7 @@ def classifyPoint(centroids,point):
 			closest = i
 			smallestDist = temp
 
+## the big function
 def cluster(df,columns,k):
 	## pick k random points from the data set
 	firstIDs = np.random.randint(np.len(df),size = k)
@@ -50,51 +79,18 @@ def cluster(df,columns,k):
 			centroids = newCentroids
 	return df
 
+## columns we wanna use
+cols = ['Time Occurred','Area ID','Reporting District','Crime Code','Victim Age','Victim Sex','Premise Code','latitude','longitude']
 
-cols = ['Time Occurred','Area ID','Area Name','Reporting District','Crime Code','Victim Age','Victim Sex','Victim Descent','Premise Code','Weapon Used Code','Status Code','Crime Code 1','Crime Code 2','Crime Code 3','Crime Code 4','Address	Cross Street','Location']
+## data cleaning
+newTimes(crime)
+newSex(crime)
+cleanAge(crime)
+
 cluster(crime,cols,50)
 
 
 
 
-
-## creating better groupings for crime code description
-## creates many dummy variables
-# def changeCrimeCode(df):
-#     if "BATTERY" in df["Crime Code Description"]:
-#         return "Battery"
-#     elif "ASSAULT" in df["Crime Code Description"]:
-#         return "Assault"
-#     elif "ASSAULT" in df["Crime Code Description"]:
-#         return "Assault"
-#     elif "ASSAULT" in df["Crime Code Description"]:
-#         return "Assault"
-#     elif "ASSAULT" in df["Crime Code Description"]:
-#         return "Assault"
-#     elif "ASSAULT" in df["Crime Code Description"]:
-#         return "Assault"
-#     elif "ASSAULT" in df["Crime Code Description"]:
-#         return "Assault"
-#     elif "ASSAULT" in df["Crime Code Description"]:
-#         return "Assault"
-#     elif "ASSAULT" in df["Crime Code Description"]:
-#         return "Assault"
-#     elif "ASSAULT" in df["Crime Code Description"]:
-#         return "Assault"
-#     elif "ASSAULT" in df["Crime Code Description"]:
-#         return "Assault"
-#     else:
-#         return "other"
-    
-#beer["style"] = beer.apply(style_func, axis=1)
-
-
-#print(len(np.unique(list(crime['Crime Code Description']))))
-
-
-
-#print (np.mean(crime['latitude']),np.mean(crime['longitude']))
-
-#'Time Occurred','Area ID','Area Name','Reporting District','Crime Code','Victim Age','Victim Sex','Victim Descent','Premise Code','Premise Description','Weapon Used Code','Weapon Description','Status Code','Status Description','Crime Code 1','Crime Code 2','Crime Code 3','Crime Code 4','Address	Cross Street','Location'
 
 
